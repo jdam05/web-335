22af366c3c848d4bac3616103412a69c97473fe4
jdam05 <j.damir05@gmail.com>
2022-10-26 01:01:56 -0400

    Assignment 2.3 inintial Submission

---
+++ week_2/damir_myworld.py
@@ -0,0 +1,10 @@
+
+#Title: damir_myworld.py
+#Date: october 26, 2022
+#Author: Jamal Eddine Damir
+#Description: Python code that prints string value
+#Sources:
+# W3Schools.com
+
+last_name = "Damir"  #Declaring last name variable and assigning a string value to it.
+print("You are now in " + last_name + "'s world!")  #Outputing string value using concatenation
